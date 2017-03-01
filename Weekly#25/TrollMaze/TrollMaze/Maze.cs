74dfa4f0b567096b81741ccec85202ffbf3c8bc8
Waagna <dominik.waagna@gmail.com>
2017-03-01 21:26:13 +0100

    Redraw the Maze so player can't get outOfBounds

--- Weekly#25/TrollMaze/TrollMaze/Maze.cs
+++ Weekly#25/TrollMaze/TrollMaze/Maze.cs
@@ -31,31 +31,74 @@ namespace TrollMaze
             //mazeField.Add("# # #");
             //mazeField.Add("#   #");
 
-            mazeField.Add("#####################################");
-            mazeField.Add("# #       #       #     #         # #");
-            mazeField.Add("# # ##### # ### ##### ### ### ### # #");
-            mazeField.Add("#       #   # #     #     # # #   # #");
-            mazeField.Add("##### # ##### ##### ### # # # ##### #");
-            mazeField.Add("#   # #       #     # # # # #     # #");
-            mazeField.Add("# # ####### # # ##### ### # ##### # #");
-            mazeField.Add("# #       # # #   #     #     #   # #");
-            mazeField.Add("# ####### ### ### # ### ##### # ### #");
-            mazeField.Add("#     #   # #   # #   #     # #     #");
-            mazeField.Add("# ### ### # ### # ##### # # # #######");
-            mazeField.Add("#   #   # # #   #   #   # # #   #   #");
-            mazeField.Add("####### # # # ##### # ### # ### ### #");
-            mazeField.Add("#     # #     #   # #   # #   #     #");
-            mazeField.Add("# ### # ##### ### # ### ### ####### #");
-            mazeField.Add("# #   #     #     #   # # #       # #");
-            mazeField.Add("# # ##### # ### ##### # # ####### # #");
-            mazeField.Add("# #     # # # # #     #       # #   #");
-            mazeField.Add("# ##### # # # ### ##### ##### # #####");
-            mazeField.Add("# #   # # #     #     # #   #       #");
-            mazeField.Add("# # ### ### ### ##### ### # ##### # #");
-            mazeField.Add("# #         #     #       #       # #");
-            mazeField.Add("#X###################################");
+            mazeField.Add("#######################################");
+            mazeField.Add("#######################################");
+            mazeField.Add("## #       #       #     #         # ##");
+            mazeField.Add("## # ##### # ### ##### ### ### ### # ##");
+            mazeField.Add("##       #   # #     #     # # #   # ##");
+            mazeField.Add("###### # ##### ##### ### # # # ##### ##");
+            mazeField.Add("##   # #       #     # # # # #     # ##");
+            mazeField.Add("## # ####### # # ##### ### # ##### # ##");
+            mazeField.Add("## #       # # #   #     #     #   # ##");
+            mazeField.Add("## ####### ### ### # ### ##### # ### ##");
+            mazeField.Add("##     #   # #   # #   #     # #     ##");
+            mazeField.Add("## ### ### # ### # ##### # # # ########");
+            mazeField.Add("##   #   # # #   #   #   # # #   #   ##");
+            mazeField.Add("######## # # # ##### # ### # ### ### ##");
+            mazeField.Add("##     # #     #   # #   # #   #     ##");
+            mazeField.Add("## ### # ##### ### # ### ### ####### ##");
+            mazeField.Add("## #   #     #     #   # # #       # ##");
+            mazeField.Add("## # ##### # ### ##### # # ####### # ##");
+            mazeField.Add("## #     # # # # #     #       # #   ##");
+            mazeField.Add("## ##### # # # ### ##### ##### # ######");
+            mazeField.Add("## #   # # #     #     # #   #       ##");
+            mazeField.Add("## # ### ### ### ##### ### # ##### # ##");
+            mazeField.Add("## #         #     #       #       # ##");
+            mazeField.Add("##X####################################");
+            mazeField.Add("#######################################");
 
             return mazeField;
+        }
+
+        /// <summary>
+        /// Sets the Cell at X Y to the given Input
+        /// </summary>
+        /// <param name="X">X Coord where the Cell is</param>
+        /// <param name="Y">Y Coord where the Cell is</param>
+        /// <param name="input">Which char should be put in the Cell</param>
+        /// <returns></returns>
+        public static bool SetCell(int X, int Y, char input)
+        {
+            if (!isValidChar(input))
+                return false;
+
+            char[] chars = null;
+            chars = field[Y].ToCharArray();
+            chars[X] = input;
+            field[Y] = new string(chars);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Returns the Value of the Cell
+        /// </summary>
+        /// <param name="X">X Coord of the Cell</param>
+        /// <param name="Y">Y Coord of the Cell</param>
+        /// <returns></returns>
+        public static char GetCell(int X, int Y)
+        {
+            return field[Y].ElementAt(X);
+        }
+
+        /// <summary>
+        /// Checks if the char is allowed in the Cell
+        /// </summary>
+        /// <param name="input">Character to Check</param>
+        /// <returns>True if allowed</returns>
+        private static bool isValidChar(char input)
+        {
+            return " #@^<>v%".Contains(input.ToString());
         }
     }
 }
